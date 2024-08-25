5637f16acc2face7c95f2c3d6f0b6cfd501f1549
Luiz Bello <luizpaulopinto25@gmail.com>
2024-08-25 11:13:12 -0300

    feat: adjust home screen

--- hierarchy-creator/src/App.tsx
+++ hierarchy-creator/src/App.tsx
@@ -1,11 +1,15 @@
-function App() {
+import HierarchyEditor from './components/HierarchyEditor/HierarchyEditor';
+import LayoutProvider from './providers/LayoutProvider';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <LayoutProvider>
+      <div className="space-y-8">
+        <h1 className="text-4xl font-bold text-center text-gray-800">Crie sua árvore</h1>
+        <HierarchyEditor />
+      </div>
+    </LayoutProvider>
   );
-}
+};
 
 export default App;
